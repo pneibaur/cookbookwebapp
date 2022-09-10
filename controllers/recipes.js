27a072de42336a49b51fcdf40b237359a6c79b4c
Phil Neibaur <pneibaur@Phils-MacBook-Pro.local>
2022-09-09 20:41:16 -0600

    adds new recipe form. adds delete button/route

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -18,9 +18,26 @@ recipeRouter.get("/", (req, res)=>{
         })
     })
 })
+//  New
+recipeRouter.get("/new", (req, res)=>{
+    res.render("recipes/new.ejs")
+})
 // Destroy
+recipeRouter.delete("/:id", (req, res)=>{
+    Recipes.findByIdAndDelete(req.params.id, ()=>{
+        res.redirect("/recipes")
+    })
+})
 // Update
 // Create
+recipeRouter.post("/", (req, res)=>{
+    Recipes.create(req.body, (error, newRecipe)=>{
+        console.log(`OG newRecipe tags: ${newRecipe.tags}`)
+        newRecipe.tags[0].split(",") // attempting to separate a string separated by comma. not functional right now. 
+        console.log(`NEW newRecipe tags: ${newRecipe}`)
+        res.redirect("/recipes")
+    })
+})
 // Edit
 // Show
 recipeRouter.get("/:id", (req, res)=>{
